5100f17e10640f1e0c4a141867450539b06e5dd6
Pravaritti <pravaritti.1995@gmail.com>
2022-01-29 16:52:14 +0530

    css manipulation and play again button

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // console.log(document.querySelector('.message').textContent);
 // console.log(document.querySelector('guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 let score = 20;
 document.querySelector('.score').textContent = score;
@@ -17,6 +17,13 @@ document.querySelector('.check').addEventListener('click', function () {
     //When the player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    //These Styles are actually set as Inline Styles = we are not actually changing CSS page directly but we are
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem'; //VALUE NEEDS TO BE A STRING here
+
+    //display number in place of ?
+    document.querySelector('.number').textContent = secretNumber;
 
     //When guess is too high
   } else if (guess > secretNumber) {
@@ -40,6 +47,20 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').textContent = ''; //empty string
+
+  //restore all intial conditions
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
 
 /*
