bc287b6fea7541fe8510dab7a130d536630a62e8
Pravaritti <pravaritti.1995@gmail.com>
2022-01-29 17:54:21 +0530

    implement highscore

--- script.js
+++ script.js
@@ -6,6 +6,8 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 let score = 20;
+let highscore = 0;
+
 document.querySelector('.score').textContent = score;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value); //number guessed by the player
@@ -21,6 +23,12 @@ document.querySelector('.check').addEventListener('click', function () {
     //These Styles are actually set as Inline Styles = we are not actually changing CSS page directly but we are
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem'; //VALUE NEEDS TO BE A STRING here
+
+    //high score
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     //display number in place of ?
     document.querySelector('.number').textContent = secretNumber;
